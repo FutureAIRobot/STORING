04303bed1dcc067dc4a4956745ec5d6e35850540
Subhan-Maxx <103839940+Subhan-Maxx@users.noreply.github.com>
2022-12-26 12:42:47 +0530

    Update start.py

--- AutoForward/plugins/start.py
+++ AutoForward/plugins/start.py
@@ -3,4 +3,4 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.command("start") & filters.incoming)
 async def start(bot, message):
-        await message.reply(" Hello, I'm Auto Forward Bot")
+        await message.reply(" Hello, I'm Auto Forward Client")
