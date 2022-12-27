bf8c06e18c7ab4151c703ce49612829d8372153a
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-27 13:49:05 +0530

    Update client.py

--- AutoForward/plugins/client.py
+++ AutoForward/plugins/client.py
@@ -7,7 +7,7 @@ from pyrogram.errors import FloodWait
 
 media_filter = filters.document | filters.video | filters.audio
  
-@Client.on_message(filters.channel & media_filter)
+@Client.on_message(filters.chat(-1001862738079) & media_filter)
 async def forward(client, update):
     try:      
         await asyncio.sleep(600)
@@ -22,19 +22,18 @@ async def forward(client, update):
     except FloodWait as e:
         await asyncio.sleep(e.value)
 
-"""
+
 @Client.on_message(filters.chat(-1001846691219) & filters.group & media_filter)
-async def forward(client, update):
+async def forward2auto(client, update):
     try:      
         await asyncio.sleep(20)
         await client.copy_message(
-            chat_id=-1001862738079,
+            chat_id=-1001427335527,
             from_chat_id=-1001846691219,
             message_id=update.id,
-            caption=update.caption,
+            caption=f"**{update.caption}**" + "\n\n" + "**@DXClassic**",
             parse_mode=enums.ParseMode.MARKDOWN
         )
 
     except FloodWait as e:
         await asyncio.sleep(e.value)
-"""
