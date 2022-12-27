dfa088b1c76410654329ad28d12c113d8118a6bc
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-27 13:24:56 +0530

    Update userforward.py

--- AutoForward/plugins/userforward.py
+++ AutoForward/plugins/userforward.py
@@ -21,3 +21,18 @@ async def forward(client, update):
 
     except FloodWait as e:
         await asyncio.sleep(e.value)
+
+@Client.on_message(filters.chat(-1001846691219) & filters.group & media_filter)
+async def forward(client, update):
+    try:      
+        await asyncio.sleep(20)
+        await client.copy_message(
+            chat_id=-1001862738079,
+            from_chat_id=-1001846691219,
+            message_id=update.id,
+            caption=update.caption,
+            parse_mode=enums.ParseMode.MARKDOWN
+        )
+
+    except FloodWait as e:
+        await asyncio.sleep(e.value)
