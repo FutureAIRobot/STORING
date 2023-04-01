555c80eaae8914987780ffbbd4826b3456cd8395
Subhan-Maxx <103839940+Subhan-Maxx@users.noreply.github.com>
2023-04-01 11:07:25 +0530

    Update info.py

--- bot/info.py
+++ bot/info.py
@@ -1,11 +1,11 @@
 from os import getenv
 
 class Config(object):      
-      ADMIN_ID = "1903280447"     
-      ADMINS = 1903280447
+      ADMIN_ID = "5490718062"     
+      ADMINS = 5490718062
       FILES_FROM_CHANNEL = -1001667023505       
       FILES_TO_CHANNEL = -1001531149575
-      FILES_CAPTION = "@HQFilms4U"
-      AUTO_FILTER_CHANNEL =  -1001743048821   
-      MULTIFORWARD_ID = list(x for x in getenv("CHANNEL_ID", "-1001793975460:-1001840022987").replace("\n", " ").split(' '))
+
+         
+
 
