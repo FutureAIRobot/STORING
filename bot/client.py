b956538547fb267f05f41dd4922149e4ca48b9e4
Subhan-Maxx <103839940+Subhan-Maxx@users.noreply.github.com>
2023-04-01 12:10:26 +0530

    Update client.py

--- bot/client.py
+++ bot/client.py
@@ -1,27 +1,34 @@
-from pyrogram import Client, __version__
 
-from bot import API_HASH, APP_ID, LOGGER, \
+
+from . import API_HASH, APP_ID, LOGGER, \
     USER_SESSION
 
+from pyrogram import Client, enums, __version__
 
-class User(Client):
+
+class User(Client):    
     def __init__(self):
         super().__init__(
-            "userrbot",
+            name="autobot",
             api_hash=API_HASH,
             api_id=APP_ID,
+            plugins={
+                "root": "AutoForward/plugins"
+            },
+            workers=200,
             session_string=USER_SESSION,
-            workers=20
+            sleep_threshold=10
         )
         self.LOGGER = LOGGER
 
     async def start(self):
         await super().start()
-        try: await self.export_session_string()
-        except: pass
-        usr_bot_me = await self.get_me()
-        return (self, usr_bot_me.id)
+        user_details = await self.get_me()
+        self.set_parse_mode(enums.ParseMode.HTML)
+        self.LOGGER(__name__).info(
+            f"@{user_details.username}  started! "
+        )
 
     async def stop(self, *args):
         await super().stop()
-        self.LOGGER(__name__).info("Bot stopped. Bye.")
+        self.LOGGER(__name__).info("User stopped. Bye.")
