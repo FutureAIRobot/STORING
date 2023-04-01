c99b6a538eaa1892561ac5bd17da0eb27f8f876e
Subhan-Maxx <103839940+Subhan-Maxx@users.noreply.github.com>
2023-04-01 11:46:34 +0530

    Update auto_forward.py

--- bot/plugins/auto_forward.py
+++ bot/plugins/auto_forward.py
@@ -1,20 +1,48 @@
 import os, asyncio
 from pyrogram.errors import FloodWait
 from pyrogram import Client, filters, enums
-from bot.importss import Config 
+ 
 
-@Client.on_message(filters.chat(-1001531149575) & (filters.media))
+# MaxplayHD Series 
+@Client.on_message(filters.chat(-1001855566600) & (filters.media))
 async def forward(bot, update):
     try:      
         await bot.copy_message(
-            chat_id=-1001641840781, 
-            from_chat_id=-1001531149575, 
+            chat_id=-1001239233167, 
+            from_chat_id=-1001855566600, 
             message_id=update.id, 
-            caption=f"**{update.caption}**".replace("Latest_Movies_Reborn", "DXClassic"),             
+            caption=update.caption,             
             parse_mode=enums.ParseMode.MARKDOWN                     
         )
     except FloodWait as e:
         await asyncio.sleep(e.value)
 
 
+# MaxplayHD Movies 
+@Client.on_message(filters.chat(-1001523497898) & (filters.media))
+async def forward(bot, update):
+    try:      
+        await bot.copy_message(
+            chat_id=-1001239233167, 
+            from_chat_id=-1001523497898, 
+            message_id=update.id, 
+            caption=update.caption,
+            parse_mode=enums.ParseMode.MARKDOWN                     
+        )
+    except FloodWait as e:
+        await asyncio.sleep(e.value)
 
+
+# New Movies 1stOnTG
+@Client.on_message(filters.chat(-1001936111628) & (filters.media))
+async def forward(bot, update):
+    try:      
+        await bot.copy_message(
+            chat_id=-1001239233167, 
+            from_chat_id=-1001936111628, 
+            message_id=update.id, 
+            caption=update.caption,
+            parse_mode=enums.ParseMode.MARKDOWN                     
+        )
+    except FloodWait as e:
+        await asyncio.sleep(e.value)
