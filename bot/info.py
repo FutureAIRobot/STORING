10a5426e2cb29504e7719afca38e3f53703acf8d
Subhan-Maxx <103839940+Subhan-Maxx@users.noreply.github.com>
2023-04-01 12:08:13 +0530

    Update info.py

--- bot/info.py
+++ bot/info.py
@@ -5,7 +5,7 @@ class Config(object):
       ADMINS = 1533128706
       FILES_FROM_CHANNEL = -1001737722424       
       FILES_TO_CHANNEL = -1001150560733
-
+      FILES_CAPTION = "@HQFilms4U"
          
 
 
